d51c76d5ca538f632e7b959c6d5b9debcb1d9180
Dmitry Zhiryakov <dmitry_zhiryakov@mail.ru>
2023-03-28 13:52:47 +0300

    Add python-dotenv and fix

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,6 +1,11 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
 
+
 app = Flask(__name__)
+load_dotenv()
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 
 @app.route('/')
