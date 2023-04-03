8f304b286fb4a5c72a31f77feb5e0f6a0e529771
Dmitry Zhiryakov <dmitry_zhiryakov@mail.ru>
2023-04-03 22:24:27 +0300

    Update app.py, database.sql. Add url.html, urls.html. Rename show.html->index.html

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,9 +1,9 @@
 import os
 import psycopg2
+from psycopg2.extras import NamedTupleCursor
 from dotenv import load_dotenv
-from flask import Flask, render_template, request
-from datetime import datetime
-
+from flask import Flask, render_template, request, redirect, url_for, flash, get_flashed_messages
+from datetime import date
 from page_analyzer.validator import validate
 
 
@@ -16,26 +16,52 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 @app.route('/')
 def index():
     return render_template(
-        'show.html'
+        'index.html'
     )
 
 
-@app.route('/urls', methods=['GET', 'POST'])
+@app.get('/urls')
+def urls_get():
+    conn = psycopg2.connect(DATABASE_URL)
+    with conn.cursor(cursor_factory=NamedTupleCursor) as curs:
+        curs.execute('SELECT id, name FROM urls ORDER BY created_at DESC;')
+        urls = curs.fetchall()
+        return render_template('urls.html',
+                               urls=urls
+                               )
+
+
+@app.post('/urls')
 def add_url():
     url = request.form['url']
 
     errors = validate(url)
     if errors:
         return render_template(
-            'show.html',
+            'index.html',
             errors=errors,
             url=url
         )
 
     conn = psycopg2.connect(DATABASE_URL)
-    with conn.cursor() as curs:
-        curs.execute('INSERT INTO urls (name, created_at) VALUES (%s, %s);',
-                     (url, datetime.today().replace(microsecond=0)))
-    conn.commit()
+    with conn.cursor(cursor_factory=NamedTupleCursor) as curs:
+        curs.execute('INSERT INTO urls (name, created_at) VALUES (%s, %s) RETURNING id;',
+                     (url, date.today()))
+        data = curs.fetchone()
+        conn.commit()
     conn.close()
-    return render_template('show.html')
+    flash('Страница успешно добавлена', 'success')
+    return redirect(url_for('show_url', id=data.id, ))
+
+
+@app.route('/urls/<int:id>', methods=['GET'])
+def show_url(id):
+    messages = get_flashed_messages(with_categories=True)
+    conn = psycopg2.connect(DATABASE_URL)
+    with conn.cursor(cursor_factory=NamedTupleCursor) as curs:
+        curs.execute('SELECT * FROM urls WHERE id = %s;', (id,))
+        url = curs.fetchone()
+        return render_template('url.html',
+                               url=url,
+                               messages=messages
+                               )
